4e4c92b09fddc1a6a7fb62eb4d2451bddd8018d0
Prasanna Reddy Pulakurthi <83247176+PrasannaPulakurthi@users.noreply.github.com>
2023-04-28 12:59:56 -0400

    Add files via upload

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import torch
 from math import log2
 
 DATASET_NAME = 'cifar10'
-MAX_IMG_SIZE = 32
+MAX_IMG_SIZE = 64
 MAX_IMG_SIZE_IDX = int(log2(MAX_IMG_SIZE)-2)
 START_TRAIN_AT_IMG_SIZE = 4
 DATASET = '../../Datasets/' + DATASET_NAME
@@ -13,7 +13,7 @@ SAVE_MODEL = True
 LOAD_MODEL = False
 LEARNING_RATE = 1e-3
 # BATCH_SIZES = [32, 32, 32, 16, 16, 16, 16, 8, 4]
-BATCH_SIZES = [32, 32, 32, 16]
+BATCH_SIZES = [32, 32, 32, 16, 16]
 CHANNELS_IMG = 3
 Z_DIM = 512  # should be 512 in original paper
 IN_CHANNELS = 512  # should be 512 in original paper
